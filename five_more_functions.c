bc119d86e11d8cecfd90d7e078a2758b40b6cce0
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-23 05:25:44 -0700

    Function hot fixes

--- five_more_functions.c
+++ five_more_functions.c
@@ -15,7 +15,8 @@ int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	char extra_c = 0, padd = ' ';
-	int ind = BUFFER_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	/* length=2, for '0x' */
+	int ind = BUFFER_SIZE - 2, length = 2, padd_start = 1;
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
 	void *addrs = va_arg(types, void *);

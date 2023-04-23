d256a73cbcd2f2a11215f80901319965202bdd3b
Jacob Kingi <jacobkingzklbz5115@gmail.com>
2023-04-22 19:57:13 -0700

    0 Hot fix

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
-	for (i = 0; format && format[i] != '\0\; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != ' % ')
 		{
